92daaa274ac3c3741a95bc31a50915f731602e3e
Pia Droop <marleneDebatin@users.noreply.github.com>
2020-02-15 23:49:28 +0100

    New Design
    Implement new design

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // load all plug-ins
-var gulp = require('gulp');
+const { watch, src, dest, series } = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
@@ -12,69 +12,65 @@ var browserSync = require('browser-sync').create();
 
 // default task is executed everytime you type gulp in the command line
 // calls the task in the array after executing
-gulp.task('default', ['copy-html', 'images', 'styles', 'scripts'], function() {
-	// watches your files for changes and calls the respective tasks
-	gulp.watch('sass/**/*.scss', ['styles']);
-	gulp.watch('js/**/*.js', ['scripts']);
-	gulp.watch('img/**', ['images']);
-	gulp.watch('*.html', ['copy-html']);
-	gulp.watch('./dist/layer.html').on('change', browserSync.reload);
-
+exports.default = series(copyHtml, images, fonts, styles, scripts, function(){
 	browserSync.init({
 		server: './dist'
 	});
 });
-// call this when you're ready for distribution!
-gulp.task('dist', [
-	'copy-html',
-	'images',
-	'styles',
-	'scripts-dist'
-]);
+watch('sass/**/*.scss', styles);
+watch('js/**/*.js', scripts);
+watch('img/**', images);
+watch('fonts/**', fonts);
+watch('*.html', copyHtml);
+watch('./dist/layer.html').on('change', browserSync.reload);
+
 // converting, concatinating and saving the Javascript to the dist folder
 // Standard Javascript task for live editing
-gulp.task('scripts', function() {
-	gulp.src('js/**/*.js')
+function scripts() {
+	return src('js/**/*.js')
 		.pipe(babel())
 		.pipe(concat('all.js'))
-		.pipe(gulp.dest('dist/js'));
-});
+		.pipe(dest('dist/js'));
+}
+
 // more time-intensive task only for distribution
 // in addition to standard js task: minimizes
-gulp.task('scripts-dist', function() {
-	gulp.src('js/**/*.js')
+function scriptsDist() {
+	return src('js/**/*.js')
 		.pipe(sourcemaps.init())
 		.pipe(babel())
 		.pipe(concat('all.js'))
 		.pipe(uglify())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('dist/js'));
-});
+		.pipe(dest('dist/js'));
+}
 // copies the HTML to the dist folder
-gulp.task('copy-html', function() {
-	gulp.src('./*.html')
-		.pipe(gulp.dest('./dist'));
-});
+function copyHtml() {
+	return src('./*.html')
+			.pipe(dest('./dist'));
+}
 // optimizes the images and saves them to the dist folder
-gulp.task('images', function() {
-	gulp.src('img/*')
-		// .pipe(imagemin({
-		//             progressive: true,
-		//             use: [pngquant()]
-		//         }))
-		  .pipe(gulp.dest('dist/img'));
-	  gulp.src('fonts/*')
-	  	.pipe(gulp.dest('dist/fonts'));
-});
+function images() {
+	return src('img/*')
+		  .pipe(dest('dist/img'));
+
+}
+function fonts() {
+	return src('fonts/*')
+	   .pipe(dest('dist/fonts'));
+}
 // converts SASS to CSS, adds prefixes and saves the files to the dist folder
-gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
+function styles() {
+	return src('sass/**/*.scss')
 		.pipe(sass({
 			outputStyle: 'compressed'
 		}).on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions']
 		}))
-		.pipe(gulp.dest('dist/css'))
+		.pipe(dest('dist/css'))
 		.pipe(browserSync.stream());
-});
+}
+
+// call this when you're ready for distribution!
+exports.dist = series(copyHtml, images, styles, scriptsDist);
